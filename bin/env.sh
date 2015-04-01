54fd550a169bf99f2f1c79f78daafcb63c55d3ee
Eric Halpern <eric.halpern@gmail.com>
2015-04-01 09:47:53 -0700

    Add test script and convenince scripts

---
+++ bin/env.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+ES_HOME=/usr/local/Cellar/elasticsearch/1.5.0
+RELEASE_DIR=$DIR/../target/releases
+TEST_SCRIPTS_DIR=$DIR/../src/test/scripts/
+
